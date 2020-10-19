d85e789109d75ff95f966edc9c0774ace0576342
annia valdes <millerl.mb79@gmail.com>
2020-10-19 13:21:25 -0500

    create type Project, the input project,defined  mutations and the User

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,46 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type Project{
+  _id:ID
+  title:String
+  ideasText:String
+  date:String
+}
 
+type User {
+    _id: ID
+    firstName:String
+    lastName:String
+    email:String    
+    projects:[Project]
+    projectCount:Int
+  }
+
+input projectInput{
+  _id:ID
+  title:String
+  ideasText:String
+  date:String
+}
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+    type Query {
+      me: User
+      users: [User]  
+  }
+
+  type Mutation {
+    addUser(firstName:String!, lastName:String!, email:String!, password:String!): Auth   
+    login(email: String!, password: String!): Auth
+    addProjects(project:projectInput):User
+    deleteProjects(_id:ID):User
+    editProjects(_id:ID):User
+  }
 `;
 
 module.exports = typeDefs;
