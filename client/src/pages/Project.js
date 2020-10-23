9a9b2246eb78ffc6aa0d82e76e345e7796d49621
ZachWagner1 <zwag14@yahoo.com>
2020-10-22 21:27:25 -0500

    basic card

---
+++ client/src/pages/Project.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Project from "./Project" 
+
+const Project = () => {
+  return (
+    <div className="container">
+      <Project/>
+    </div>
+  );
+};
+
+export default Project;
