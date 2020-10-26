f5c7edd43e3e473a798affd0e82c1ff2029a1b08
annia valdes <millerl.mb79@gmail.com>
2020-10-26 11:35:49 -0500

    update de handleDeleteProject

--- client/src/components/Project/index.js
+++ client/src/components/Project/index.js
@@ -1,57 +1,62 @@
 import React, { useState } from "react";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 import { ME } from "../../utils/queries";
-import { EDIT_PROJECT, DELETE_PROJECT } from "../../utils/mutations";
+import { DELETE_PROJECT } from "../../utils/mutations";
 
 
 
 function Project() {
 
   const { loading, data } = useQuery(ME);
-
-  const [project, setProjectInfo] = useState({ title: "", ideasText: "" });
-  const { title, ideasText } = project;
-
-
+ 
   const [deleteProject, { error }] = useMutation(DELETE_PROJECT);
 
   const userData = data?.me || {};
 
-  const handleDeleteProject = async(id) => {
- console.log("ID to delete")
+  const handleDeleteProject = async (_id) => {
+    console.log("ID to delete", _id)
+
     try {
-      const {data} = await deleteProject(id);
-      deleteProject(id);
+      await deleteProject({ variables: { _id } });
+      window.location.reload();
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-    }  catch(err) {
-    console.error(err);
+  console.log("Query user", userData.projects);
+
+  if (!loading) {
+    console.log(userData);
   }
-};
+  return(
+    <>
+    <h5>{userData.firstName} {userData.lastName} have {userData.projectCount} project(s)</h5>
+     {
+       !loading ? userData.projects.map((my) => {
+       return (
+ 
+         <div key={my._id} className="row">          
+           <div className="col s12 m6">
+             <div className="card blue-grey darken-1">
+               <div className="card-content white-text">
+                 <span className="card-title">{my.title}</span>
+                 <p>{my.ideasText}</p>
+               </div>
+               <div className="card-action">
+                 <button >Edit</button>
+                 <button onClick={() => handleDeleteProject(my._id)}>Delete</button>
+               </div>
+             </div>
+           </div>
+         </div>
+       )      
+       }) : ""}
+ 
+   </>
+  )
+ 
 
-console.log("Query user", userData.projects);
-
-if (!loading) {
-  console.log(userData);
 }
-return !loading
-  ? userData.projects.map((my) => (
-    <div key={my._id} class="row">
-      <div class="col s12 m6">
-        <div class="card blue-grey darken-1">
-          <div class="card-content white-text">
-            <span class="card-title">{my.title}</span>
-            <p>{my.ideasText}</p>
-          </div>
-          <div class="card-action">
-            <button >Edit</button>
-            <button onClick={() => handleDeleteProject(my._id)}>Delete</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  ))
-  : "";
-}
 
-// onClick={(event) =>this.deleteEntry(event, entry._id)}
 export default Project;
